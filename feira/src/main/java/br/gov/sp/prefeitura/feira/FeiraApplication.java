05cc9ccf3cb7c21824842f1e671e272f8b81c32d
Igor Guilherme <ifgandrade@outlook.com>
2021-05-02 10:22:42 -0300

    Primeiro commit

---
+++ feira/src/main/java/br/gov/sp/prefeitura/feira/FeiraApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.prefeitura.feira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeiraApplication.class, args);
+	}
+
+}
